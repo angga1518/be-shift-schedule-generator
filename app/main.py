fb503f3d3bf1904ba4e55dd981ddd6fb5d8e6775
Muhammad Erlangga <muhammad.erlangga@oyindonesia.com>
2025-08-07 07:49:34 +0700

    handle cors

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .api import endpoints
+
 
 app = FastAPI(
     title="Medical Shift Schedule Generator",
@@ -7,8 +10,24 @@ app = FastAPI(
     version="1.0.0"
 )
 
+# CORS settings
+origins = [
+    "http://localhost:3000",
+    "https://laras-shift.erlangga.xyz"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
 # Include the API router
 app.include_router(endpoints.router, prefix="/api")
+
 
 @app.get("/", tags=["Root"])
 async def read_root():
